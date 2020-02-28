f59e5cb33f126409ac0e42def1f17ac2e258d4bf
BillXuce <xuce0915@vip.qq.com>
2020-02-28 14:52:28 +0800

    a -> f

--- names.py
+++ names.py
@@ -1,6 +1,6 @@
 names=[]
-with open('names.txt', encoding="utf-8") as a:
-    for line in a.readlines():
+with open('names.txt', encoding="utf-8") as f:
+    for line in f.readlines():
         name=line.split()
         names.append(name[0])
 print(names)
